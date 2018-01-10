195a5840871142846bd5e08a5cd12d73c3272546
Raphael Tony <prince4raphael@gmail.com>
2018-01-10 23:17:46 +0530

    Added search bar

--- app/components/tabs/search.js
+++ app/components/tabs/search.js
@@ -1,9 +1,16 @@
 import React from 'react'
-import {Container} from 'native-base';
+import { Container, Header, Item, Icon, Input, Button } from 'native-base';
 import { Text } from 'react-native';
 
 const Search = () => (
-    <Text>Sample Content</Text>
+    <Container>
+
+        <Item>
+            <Icon name="md-arrow-back" style={{ color: '#3BB9FF', paddingHorizontal: 15 }} />
+            <Input placeholder="Search Twitter" />
+        </Item>
+
+    </Container>
 );
 
 export default Search;
